f0f6bf21387cec69588bee56be3173b01cec7571
saraansh <gannasaraansh@gmail.com>
2026-06-24 17:07:21 -0700

    dsf

--- Assets/UniversalScripts/DEVSCRIPTS.cs
+++ Assets/UniversalScripts/DEVSCRIPTS.cs
@@ -7,7 +7,7 @@ public class DEVSCRIPTS : MonoBehaviour
 {
     private void Update()
     {
-        if ((Input.GetKey(KeyCode.M)) && (Input.GetKey(KeyCode.O)) && (Input.GetKey(KeyCode.N)) && (Input.GetKey(KeyCode.K)) && (Input.GetKey(KeyCode.E)))
+        if ((Input.GetKey(KeyCode.M)) && (Input.GetKey(KeyCode.O)) && (Input.GetKey(KeyCode.N)) && (Input.GetKey(KeyCode.Y)) && (Input.GetKey(KeyCode.E)))
         {
             SceneManager.LoadScene(11);
         }
